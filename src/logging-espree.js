c27f5de1a6b2478df9f05befbd226d1c89b30160
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2023-02-28 09:54:08 +0000

    feat: adding function line (#5)

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -14,7 +14,7 @@ export async function transpile(inputFile, outputFile) {
  * @returns {string} The JavaScript code with logging statements added
  */
 function addLogging(code) {
-  const ast = espree.parse(code, { ecmaVersion: 2020 });
+  const ast = espree.parse(code, { ecmaVersion: 2020,  loc: true });
   estraverse.traverse(ast, {
     enter: function(node, _) {
       if (/\w*Function\w*/.test(node.type)) addBeforeCode(node);
@@ -31,7 +31,8 @@ function addLogging(code) {
 function addBeforeCode(node) {
   const name = node.id?.name ?? '<anonymous function>';
   const params = node.params.map((p) => `\${ ${p.name} }`).join(', ');
-  const beforeCode = `console.log('Entering ${name}(${params})');`; // :) <- Estela made this
+  const { line } =  node.loc.start;
+  const beforeCode = `console.log('Entering ${name}(${params}) at line ${line}');`; // :) <- Estela made this
   const beforeNodes = espree.parse(beforeCode).body;
   node.body.body = [ ...beforeNodes, ...node.body.body ];
 }
