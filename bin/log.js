670d0c541c7d15c287e7b31e725766b8bc9785d7
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2023-02-28 09:45:38 +0000

    feat: line commmands (#1)

--- bin/log.js
+++ bin/log.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-import { program } from "commander";
-import { createRequire } from "module";
+import { program } from 'commander';
+import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const { version } = require("../package.json");
-import { transpile } from "../src/logging-espree.js";
+const { version } = require('../package.json');
+import { transpile } from '../src/logging-espree.js';
 
 program
   .version(version)
-  .argument("<filename>", 'file with the original code')
-  .option("-o, --output <filename>", "file in which to write the output")
+  .argument('<filename>', 'file with the original code')
+  .option('-o, --output <filename>', 'file in which to write the output')
   .action((filename, options) => {
     transpile(filename, options.output);
   });
