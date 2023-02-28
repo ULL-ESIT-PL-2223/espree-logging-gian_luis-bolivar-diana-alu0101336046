56a053d53cf0afc8c39a1012c49fad8d9f2d6e05
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2023-02-28 09:51:44 +0000

    feat: logs statements at the beginning of functions (#2 and #3)

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -7,10 +7,30 @@ export async function transpile(inputFile, outputFile) {
   // Fill in the code here
 }
 
-export function addLogging(code) {
-  // Fill in the code here
+/**
+ * @description Adds logging statements at the beginning of any function.
+ *
+ * @param {string} code The code to process.
+ * @returns {string} The JavaScript code with logging statements added
+ */
+function addLogging(code) {
+  const ast = espree.parse(code, { ecmaVersion: 2020 });
+  estraverse.traverse(ast, {
+    enter: function(node, _) {
+      if (/\w*Function\w*/.test(node.type)) addBeforeCode(node);
+    }
+  });
+  return escodegen.generate(ast);
 }
 
+/**
+ * @description Adds logging code to the beginning of a function.
+ *
+ * @param {Node} node The node to add the logging code to.
+ */
 function addBeforeCode(node) {
- // Fill in the code here
+  const name = node.id?.name ?? '<anonymous function>';
+  const beforeCode = `console.log('Entering ${name}()');`; // :) <- Estela made this
+  const beforeNodes = espree.parse(beforeCode).body;
+  node.body.body = [ ...beforeNodes, ...node.body.body ];
 }
